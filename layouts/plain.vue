5247e9655347bd271d003f4650a9d18d1653360a
Ricardo Nascimento <rncnascimento@gmail.com>
2020-07-28 14:52:27 +0100

    plain no-css

---
+++ layouts/plain.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>
+      <nuxt-link to="/">No Functional</nuxt-link>
+      <nuxt-link to="/functional">Functional</nuxt-link>
+      <nuxt-link to="/static">Static</nuxt-link>
+      <nuxt-link to="/content">Content</nuxt-link>
+      <nuxt-link to="/lazy">Lazy</nuxt-link>
+      <a href="/static-render">Static Render Component</a>
+      <nuxt-link to="/plain">Plain</nuxt-link>
+    </div>
+    <Nuxt />
+  </div>
+</template>
