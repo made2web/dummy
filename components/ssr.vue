fbf65aa7cad4eb964ac725929223efb580037321
Ricardo Nascimento <rncnascimento@gmail.com>
2020-07-28 17:55:40 +0100

    ssr test

---
+++ components/ssr.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-html="message"></div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    message: String,
+  },
+  beforeUpdate() {
+    console.time();
+  },
+
+  updated() {
+    console.log("Time for render:");
+    console.timeEnd();
+  },
+  mounted() {
+    // If we didn't already do it on the server
+    // we fetch the item
+    if (!this.message) {
+      console.log("aaaa");
+    }
+  },
+});
+</script>
--- pages/plain.vue
+++ pages/plain.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <dummy :message="message" />
+    <ssr :message="message" />
   </div>
 </template>
 
@@ -14,6 +14,14 @@ export default Vue.extend({
       message:
         "<p>Existem inúmeras opções terapêuticas para o cancro da próstata que apresentam bons resultados.</p><p>A maioria dos homens diagnosticados com um tumor neste órgão pode esperar viver muitos anos. Embora o sucesso do tratamento dependa, naturalmente, da progressão da doença oncológica.</p><p>Se for detectado em fases precoces, localizado ainda apenas na próstata, o tratamento tem fins curativos. A braquiterapia, a cirurgia e a radioterapia externa são as opções terapêuticas possíveis, dado o tumor estar ainda numa fase precoce.</p><p>Se o cancro for diagnosticado em fases avançadas, ou seja, já não estiver localizado apenas na próstata, os tratamentos são mais agressivos. Nestes casos, têm um intuito paliativo. Procura-se controlar a doença, impedir a sua progressão e minimizar os sintomas.</p><p>Por isso, e como regra, quanto mais precocemente se diagnosticar um cancro da próstata, maior será a eficácia do tratamento, menores serão as consequências do tumor e dos seus tratamentos, menores as taxas de complicações e os efeitos secundários.</p>",
     };
+  },
+  mounted() {
+    // If we didn't already do it on the server
+    // we fetch the item
+    if (!this.message) {
+      this.message = "teste";
+      console.log("entrou aqui");
+    }
   },
 });
 </script>
